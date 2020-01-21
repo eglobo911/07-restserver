7d693044506a734c2108721b15ebb1e68312100b
eglobo911 <eglobo911@gmail.com>
2020-01-21 13:26:28 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://strider:w0RNQy3geNG6xKq2@cluster0-ehvg4.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 };
 
 process.env.URLDB = urlDB;
